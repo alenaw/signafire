44f9244bfb18b40f33416936f028b9919ef228f1
Alena Wang <alena.n.wang@gmail.com>
2018-09-06 00:46:48 -0400

    Add basic validations on models

--- app/models/elastic_index.rb
+++ app/models/elastic_index.rb
@@ -1,3 +1,4 @@
-class User < ApplicationRecord
+class ElasticIndex < ApplicationRecord
   has_and_belongs_to_many :users
+  validates :name, presence: true, uniqueness: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ApplicationRecord
-  has_and_belongs_to_many :elastic_indexes
+  has_and_belongs_to_many :elastic_indices
+  validates :name, presence: true, uniqueness: true
 end
