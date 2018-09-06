bb4fd6e8cb2015c93a5f0ca56b1c2f0022ea5f17
Alena Wang <alena.n.wang@gmail.com>
2018-09-06 09:36:17 -0400

    Add seeds for sqlite and elasticsearch databases

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
-  get '/users/:name', to: 'users#read'
+  get  '/users/:name', to: 'users#read'
+  get  '/results',     to: 'elastic_results#query'
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,30 @@
 # Seed sqlite database with users and elastic index names
-elastic_indices = %w(foo_index bar_index baz_index buzz_index)
-
-elastic_indices.each do |name|
-  ElasticIndex.create(name: name)
-end
+foo_index  = ElasticIndex.create(name: 'foo_index')
+bar_index  = ElasticIndex.create(name: 'bar_index')
+baz_index  = ElasticIndex.create(name: 'baz_index')
+buzz_index = ElasticIndex.create(name: 'buzz_index')
 
 %w(foo buzz).each do |name|
-  User.create(name: name)
+  User.create(
+    name: name,
+    elastic_indices: [foo_index, bar_index, baz_index, buzz_index]
+  )
 end
 
-# Seed elasticsearch database with results
+bar_index = User.create(name: 'bar', elastic_indices: [bar_index])
+baz_index = User.create(name: 'baz', elastic_indices: [baz_index])
+
+# Seed elasticsearch database with documents
+documents = YAML.load_file('db/elasticsearch_seeds.yaml')
+uri = URI(Rails.application.config.elasticsearch_url)
+http = Net::HTTP.new(uri.host, uri.port)
+
+documents.each do |document|
+  index = document.delete('index')
+  id = document.delete('id')
+  request = Net::HTTP::Put.new(
+    "/#{index}/docs/#{id}",
+    {'Content-Type' => 'application/json'}
+  )
+  http.request(request, document.to_json)
+end
